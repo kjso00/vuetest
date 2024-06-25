5fc6d3dfb799a3805cbe25576d9f6c00c56c3815
kjsoo00 <wanns2085@gmail.com>
2024-06-25 15:24:51 +0900

    first commit

---
+++ 09_VueBlogWithJson/src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/post">Post</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  background-color: #f8f9fa;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  width: 100%;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+a {
+  margin: 0 15px;
+  font-size: 18px;
+  color: #007bff;
+  text-decoration: none;
+  transition: color 0.3s ease-in-out;
+}
+
+a:hover {
+  color: #0056b3;
+}
+
+a.active {
+  font-weight: bold;
+  border-bottom: 2px solid #0056b3;
+}
+
+.RouterLink {
+  margin-left: 10px;
+  font-size: 20px;
+}
+
+</style>
---
+++ 09_VueBlogWithJson/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import TheWelcom from '@/components/home/TheWelcom.vue';
+</script>
+
+<template>
+    <main>
+        <TheWelcom/>
+    </main>
+</template>
+
+<style scoped>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 70%;
+    margin-top: 1.5%;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px dotted;
+    background-color: #f0f8ff; /* 배경색 추가 */
+    padding: 20px; /* 내용물과의 간격을 위해 패딩 추가 */
+    border-radius: 10px; /* 모서리를 둥글게 */
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* 그림자 효과 추가 */
+  }
+</style>
